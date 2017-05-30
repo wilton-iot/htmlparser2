bcc32feb26aa2555575d5de6214f7257b023d076
alex <alex@staticlibs.net>
2017-05-30 23:21:40 +0100

    test globals cleanup

--- test/api.js
+++ test/api.js
@@ -1,4 +1,7 @@
 define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+var test = require("tape-compat");
+var describe = test.describe;
+var it = test.it;    
 var htmlparser2 = require("htmlparser2"),
     assert = require("assert");
 
